fc24c7202c37eb37722169819fc4e0d97c8e83cd
Elsharabasy73 <abdo.make73@gmail.com>
2025-03-07 23:17:29 +0200

    Add mongoose and morgan for database connection and logging

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+
 dotenv.config({ path: "config.env" });
+const PORT = process.env.PORT || 3001;
+
 const app = express();
 
-const PORT = process.env.PORT || 3001;
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+  console.log(`Mode: ${process.env.NODE_ENV}`);
+}
 
 app.get("/", (req, res) => {
   res.send("Hello World");
@@ -12,3 +20,13 @@ app.get("/", (req, res) => {
 app.listen(3000, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+mongoose
+  .connect(process.env.DB_URI)
+  .then((conn) => {
+    console.log(`DB connected: ${conn.connection.host}`);
+  })
+  .catch((err) => {
+    console.log(`DB connection failed: ${err}`);
+    process.exit(1);
+  });
